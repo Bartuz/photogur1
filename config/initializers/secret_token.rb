168b7942fe740187abf1f7bf0b48e483ba43c2d0
Bartuz <iamnotawowplayer@gmail.com>
2013-07-04 17:55:38 -0400

    add rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photogur1::Application.config.secret_token = '163b364a902bb918624ba43656e08715158e30896ada7085bf96700063bda9794b07b2d8c5fda71a16d892f5cc2f4ff0f4d36527d362f89603efaf6514eb4f82'
